6a14a7d5a05006900ab671f26217e0ae55f4fccc
woodmurderedhat <imurderedthatunicorn@gmail.com>
2025-05-10 01:59:30 +0200

    dd

---
+++ games/generative-art/js/animation.js
@@ -0,0 +1,7 @@
+/**
+ * animation.js - Re-exports from the animation directory
+ * This file is kept for backward compatibility
+ */
+
+// Re-export from the animation directory
+export * from './animation/index.js';
